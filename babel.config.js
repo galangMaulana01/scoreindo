8bbe50e13e595e1c5c206806f6dc499776b61ef6
galangMaulana01 <maulanagebril99@gmail.com>
2026-06-10 19:44:10 +0700

    update

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      // react-native-reanimated/plugin TIDAK perlu ditambah manual
+      // karena Reanimated v4 (react-native-worklets) sudah include sendiri
+    ],
   };
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./App.{js,jsx,ts,tsx}", 
-    "./screens/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}"
+    "./App.{js,jsx,ts,tsx}",
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
   ],
+  presets: [require("nativewind/preset")],
   theme: {
-    extend: {
-      colors: {
-        merah: "#FC0B12",
-        kuning: "#F7CC0C",
-        latar: "#121212",
-        kartu: "#191919",
-      }
-    }
+    extend: {},
   },
   plugins: [],
-}
+};
